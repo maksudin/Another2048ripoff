1dcf8174369ca252d3f56659816af69c9d26638d
maksudin <m.iljushkin2@yandex.ru>
2017-09-04 11:02:10 +0300

    2048 game works just fine

---
+++ Assets/Scripts/TileStyleHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Which means we will see our styles in unity inspector
+[System.Serializable]
+public class TileStyle {
+	public int Number;
+	public Color32 TileColor;
+	public Color32 TextColor;
+}
+
+public class TileStyleHolder : MonoBehaviour {
+
+	// Singleton
+	public static TileStyleHolder Instance;
+
+	public TileStyle[] TileStyles;
+
+	void Awake() {
+		Instance = this;
+	}
+
+}
